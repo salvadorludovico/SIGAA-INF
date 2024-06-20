9fa1da41fa5b0175c4e0ce3f0a24344a19bd829f
Salvador Ludovico <49698940+salvadorludovico@users.noreply.github.com>
2024-06-20 11:44:07 -0300

    db create tables by pure sql removed

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,6 @@ import sqlite3 from 'sqlite3'
 import { open } from 'sqlite'
 import { Sequelize } from 'sequelize'
 
-// you would have to import / invoke this in another file
 export async function openDb () {
   return open({
     filename: './database.db',
@@ -43,63 +42,3 @@ const sequelize = new Sequelize({
 });
 
 export default sequelize;
-
-// export async function createAllTables () {
-//   const db = await openDb();
-
-//   try {
-//     await db.exec(`
-//       CREATE TABLE usuarios (
-//       id INTEGER PRIMARY KEY AUTOINCREMENT,
-//       nome TEXT NOT NULL,
-//       nascimento DATE NOT NULL,
-//       email TEXT NOT NULL UNIQUE,
-//       senha TEXT NOT NULL,
-//       curso TEXT NOT NULL CHECK (curso IN ('CC', 'SI', 'IA', 'ES')),
-//       docente BOOLEAN DEFAULT FALSE,
-//       coordena_curso TEXT CHECK (coordena_curso IN ('CC', 'SI', 'IA', 'ES', '')),
-//       vice_diretor BOOLEAN DEFAULT FALSE,
-//       status TEXT NOT NULL CHECK (status IN ('ATIVO', 'INATIVO', 'TRANCADO')));  
-//     `)
-
-//     await db.exec(`
-//       CREATE TABLE disciplina (
-//       id INTEGER PRIMARY KEY AUTOINCREMENT,
-//       ementa TEXT NOT NULL,
-//       ch_teorica INTEGER NOT NULL,
-//       ch_pratica INTEGER NOT NULL,
-//       curso TEXT NOT NULL CHECK (curso IN ('CC', 'SI', 'IA', 'ES')));
-//     `)
-
-//     await db.exec(`
-//       CREATE TABLE turma (
-//       id INTEGER PRIMARY KEY AUTOINCREMENT,
-//       disciplina_id INTEGER NOT NULL,
-//       letra CHAR(1) NOT NULL,
-//       semestre_id INTEGER NOT NULL,
-//       plano_id INTEGER NOT NULL,
-//       docente_responsavel INTEGER NOT NULL,
-//       docente_secundario INTEGER,
-//       horario TEXT NOT NULL,
-//       local TEXT NOT NULL,
-//       max_alunos INTEGER NOT NULL,
-//       chave_acesso TEXT NOT NULL,
-//       FOREIGN KEY (disciplina_id) REFERENCES disciplina(id));
-//     `)
-
-//     await db.exec(`
-//       CREATE TABLE discente_turma (
-//       turma_id INTEGER NOT NULL,
-//       aluno_id INTEGER NOT NULL,
-//       status TEXT NOT NULL CHECK (status IN ('CURSANDO', 'APROVADO', 'REPROVADO', 'TRANCADO')),
-//       PRIMARY KEY (turma_id, aluno_id),
-//       FOREIGN KEY (turma_id) REFERENCES turma(id),
-//       FOREIGN KEY (aluno_id) REFERENCES usuarios(id));
-//     `)
-
-//     db.close();
-//   } catch (error) {
-//     console.error('Error creating tables:', error);
-//   }
-// }
-
